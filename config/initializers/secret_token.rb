b8d03853cac846e26d74828a7c35b4fe0440a4fb
Garrett Flanagan <ecomoperations@gmail.com>
2015-05-30 14:37:17 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bowler::Application.config.secret_key_base = 'b8131d4c1954f751f3ca7c823daa5f62f9482713f3380ff722d53785661166313960ff818bbdd3ed71dab93bed64f60fcf313319ba3ea08a88f5e133efcfafae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bowler::Application.config.session_store :cookie_store, key: '_bowler_session'
